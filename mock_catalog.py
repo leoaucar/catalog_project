104cd37077eb7d8c6b72f108681ad01414cad4aa
Leonardo Aucar <leonardoaucar@gmail.com>
2018-12-22 19:33:44 -0200

    filled the database with more categories and itens

--- mock_catalog.py
+++ mock_catalog.py
@@ -33,6 +33,7 @@ session.commit()
 
 
 #create 3 fake categories and its itens
+
 category1 = Category(name='Cores', description='todas as cores do mundo', last_edit=datetime.datetime.now(), user=user1)
 
 session.add(category1)
@@ -41,4 +42,34 @@ session.commit()
 item1 = Item(name='Azul', description='da cor do mar', price=24.5, category=category1, last_edit=datetime.datetime.now(), user=category1.user)
 
 session.add(item1)
+session.commit()
+
+item2 = Item(name='Verde', description='da cor da flores', price=29.5, category=category1, last_edit=datetime.datetime.now(), user=category1.user)
+
+session.add(item2)
+session.commit()
+
+item3 = Item(name='Vermelho', description='da cor do coracao', price=39.9, category=category1, last_edit=datetime.datetime.now(), user=category1.user)
+
+session.add(item3)
+session.commit()
+
+category2 = Category(name='Dias', description='da semana', last_edit=datetime.datetime.now(), user=user2)
+
+session.add(category2)
+session.commit()
+
+item1_dia = Item(name='Segunda', description='Pior dia', price=9.99, category=category2, last_edit=datetime.datetime.now(), user=category2.user)
+
+session.add(item1_dia)
+session.commit()
+
+item2_dia = Item(name='Domingo', description='Fantastico!', price=0.9, category=category1, last_edit=datetime.datetime.now(), user=category1.user)
+
+session.add(item2_dia)
+session.commit()
+
+item3_dia = Item(name='Sexta', description='Dia da maldade', price=99.9, category=category1, last_edit=datetime.datetime.now(), user=category1.user)
+
+session.add(item3_dia)
 session.commit()
